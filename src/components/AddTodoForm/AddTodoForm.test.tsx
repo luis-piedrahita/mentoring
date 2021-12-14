f088c8f29b4f1425a4c6fcf5bba88f97feff29cc
Luis <luis.piedrahita@autodesk.com>
2021-12-14 15:15:17 -0300

    test(tests): addtodoform test init

---
+++ src/components/AddTodoForm/AddTodoForm.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { AddTodoForm } from './AddTodoForm';
+
+describe('AddTodoForm', () => {
+  it('render the onlcik button correctly', () => {
+    // const root = render(<AddTodoForm addTodo= />);
+    // expect(root).toBeTruthy();
+  });
+});
